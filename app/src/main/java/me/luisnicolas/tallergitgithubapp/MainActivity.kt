b4f6876d5902419fd791cf39b5c11ff761baf3bb
Nicolas Bernal <lbernal818@unab.edu.co>
2026-09-16 15:43:42 -0500

    feat: agrega descripcion de intereses academicos y profesionales

--- app/src/main/java/me/luisnicolas/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/luisnicolas/tallergitgithubapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.luisnicolas.tallergitgithubapp
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
@@ -24,6 +25,7 @@ fun ProfileScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
